5b74ed7268e819b1494ea8ec7bf9e5b875ea77ff
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-12-23 15:57:21 +0530

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // Declare a new state varaible, which we'll call "count"
+  // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
 
   return (
